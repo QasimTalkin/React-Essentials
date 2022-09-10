1f3a5b14d43164334e8b67b12c8238d7f07adeec
Abul Qasim <abulqasim110@gmail.com>
2022-09-10 15:02:14 -0400

    some fix

--- src/components/Organism/About.tsx
+++ src/components/Organism/About.tsx
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <section>
           <h1 className="text-3xl font-bold underline">
-      Welcome a
+      Welcome Abul
     </h1>
       <img
         src="https://images.unsplash.com/photo-1561336313-0bd5e0b27ec8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80"
